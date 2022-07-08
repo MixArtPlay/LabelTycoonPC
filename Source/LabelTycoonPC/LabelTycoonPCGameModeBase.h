a87e4f1bd4521db0044d29b16229ee8b20b60968
MixArtPlay <mixartapp@gmail.com>
2022-07-08 18:45:25 +0900

    Version 0.0.1

---
+++ Source/LabelTycoonPC/LabelTycoonPCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabelTycoonPCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABELTYCOONPC_API ALabelTycoonPCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
